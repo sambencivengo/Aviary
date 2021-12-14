ca3af179a52720982eeb39872dd43736cd2c1bff
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-14 10:21:28 -0500

    routes+auth  set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create, :index]
+
+  # def create
+  #   user = User.create!(user_params)
+  #   session[:user_id] = user.id
+  #   render json: user, status: :created
+  # end
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create 
+    user = User.create(user_params)
+    if user.valid?
+        session[:user_id] = user.id
+        render json: user, status: :created 
+    else 
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity 
+    end 
+  end
+
+
+  def show
+    render json: current_user
+  end
+
+
+  private 
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
