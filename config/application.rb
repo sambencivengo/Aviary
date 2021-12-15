c00b675b7580cc0235ee08f765a541025bbb2886
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-15 09:41:14 -0500

    before scrape

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module NewBackend
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-
+    
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
   end
