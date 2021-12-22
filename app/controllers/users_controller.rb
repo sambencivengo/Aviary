d16f3b54aa4846b653fea1373795c516d687133f
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-22 16:19:56 -0500

    got rid of login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   def create 
     user = User.create(user_params)
     if user.valid?
-
         render json: user, status: :created 
     else 
         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity 
