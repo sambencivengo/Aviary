bf4eb7cb27fbe4eb499239513907e1d2a64c2878
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-22 12:38:19 -0500

    adding follows table

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,19 +2,18 @@ class FollowsController < ApplicationController
   
 
   def create
-    follow = Follow.create
+    follow = Follow.create(follow_params)
+    render json: follow
   end
-
-
 
   private
 
 
   # params necessary? This action will only happen on button click after the log in portal is passed
 
-  # def follow_params
-  #   params.permit(:)
+  def follow_params
+    params.permit(:follower_id, :followed_id)
     
-  # end
+  end
 
 end
