dbefe0af318045c34424cec5ce27e471cfb57102
Sam Bencivengo <sambencivengo@gmail.com>
2022-01-17 19:36:38 -0500

    removed fallback

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,7 +1,7 @@
 class FallbackController < ApplicationController
   skip_before_action :authorize
-  
-  def index
-    render json:{ error: "No way, pal!"}
-  end
+
+  # def index
+  #   render json:{ error: "No way, pal!"}
+  # end
 end
